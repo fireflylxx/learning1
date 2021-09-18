305dcb2200a92607209b7cfaa7f357434acee842
fireflylxx <90123925+fireflylxx@users.noreply.github.com>
2021-09-18 21:06:18 +0800

    Update keyCount.py
    改正了代码当中一些书写的不规范

--- keyCount.py
+++ keyCount.py
@@ -23,11 +23,13 @@ KEYWORDS = (
     "float", "for", "goto", "if"
 )
 
+
 # 对文本进行处理，导入re，筛
-def REMAKE(PATH):
-    data = open(PATH, mode='r').read()
-    data_final = re.sub(r"\/\*([^\*^\/]*|[\**\/*]*|[^\**\/]*)*\*\/", "", data)  # 剔除注释块
-    data_final = re.sub(r"\/\/[^\n]*", "", data_final)                          # 剔除字符串
+def remake(path):
+    data = open(path, mode='r').read()
+    data_final = re.sub(r"\/\*([^\*^\/]*|[\*^\/*]*|[^\**\/]*)*\*\/", "", data)               # 剔除注释块
+    data_final = re.sub(r"[^\n]*", "", data_final)                              # 剔除注释行
+    data_final = re.sub(r"\"(.*)\"", "", data_final)                            # 剔除字符串
     data_final = re.sub(r"[ \f\r\t\v]+", " ", data_final)                       # 剔除多余空格
     data_final = re.sub(r"[\n]+", "  ", data_final)                             # 替换换行为双空格
     data_final = re.sub(r"[{]+", "  ", data_final)                              # 替换大括号为双空格
@@ -36,14 +38,15 @@ def REMAKE(PATH):
     data_final = re.split(r"\W", data_final)                                    # 转成列表
     return data_final
 
-def FIND_KEYWORDS(data_final):
+
+def find_keywords(data_final):
     global STACK, KEY_COUNT                                                     # 全局变量声明
     global CASE_COUNT, SWITCH_COUNT
     global IF_ELSE_COUNT
     global IF_ELIF_ELSE_COUNT
 
-    DATA_ITER = iter(range(len(data_final)))                                    # 生成一个可迭代对象
-    for i in DATA_ITER:                                                         # 和字典匹配查找
+    data_iter = iter(range(len(data_final)))                                    # 生成一个可迭代对象
+    for i in data_iter:                                                         # 和字典匹配查找
         temp = data_final[i]
         if temp != '' and temp in KEYWORDS:                                     # GRADE 1
             KEY_COUNT += 1
@@ -51,7 +54,7 @@ def FIND_KEYWORDS(data_final):
             if temp == "switch":                                                # GRADE 2
                 SWITCH_COUNT += 1
                 if CASE_COUNT[0] != 0:
-                   CASE_COUNT.append(0)
+                    CASE_COUNT.append(0)
 
             elif temp == "case":
                 CASE_COUNT[-1] += 1
@@ -63,7 +66,7 @@ def FIND_KEYWORDS(data_final):
                 if data_final[i+1] == "if":
                     STACK.append('elif')
                     KEY_COUNT += 1
-                    DATA_ITER.__next__()
+                    data_iter.__next__()
 
                 else:
                     elif_flag = False
@@ -77,32 +80,35 @@ def FIND_KEYWORDS(data_final):
                     else:
                         IF_ELSE_COUNT += 1
 
+
 # 输出对应等级函数
-def OUT_PUT(GRADE):
-     if GRADE >= 1:                                                     # python里无switch case 语句
-       print("total num:", KEY_COUNT)
+def out_put(grade):
 
-     if GRADE >= 2:
-         print("switch num:", SWITCH_COUNT)
-         print("case num:", end=' ')
-         if SWITCH_COUNT > 0:
-             print(*CASE_COUNT, sep=' ')
+    if grade >= 1:                                                     # python里无switch case 语句
+        print("total num:", KEY_COUNT)
 
-         else:
-             print(0)
+    if grade >= 2:
+        print("switch num:", SWITCH_COUNT)
+        print("case num:", end=' ')
+        if SWITCH_COUNT > 0:
+            print(*CASE_COUNT, sep=' ')
 
-     if GRADE >= 3:
-         print("if-else num:", IF_ELSE_COUNT)
+        else:
+            print(0)
 
-     if GRADE >= 4:
-         print("if-elseif-else num:", IF_ELIF_ELSE_COUNT)
+    if grade >= 3:
+        print("if-else num:", IF_ELSE_COUNT)
+
+    if grade >= 4:
+        print("if-elseif-else num:", IF_ELIF_ELSE_COUNT)
+
 
 # 主函数
 if __name__ == "__main__":
+
     PATH = sys.argv[1]                                                        # 传入文件名
     str2 = sys.argv[2]                                                        # 传入等级并且转换类型
     GRADE = int(str2)
-    FILE = REMAKE(PATH)                                                       # 列表化
-    FIND_KEYWORDS(FILE)                                                       # 处理
-    OUT_PUT(GRADE)                                                            # 输出
-
+    FILE = remake(PATH)                                                       # 列表化
+    find_keywords(FILE)                                                       # 处理
+    out_put(GRADE)                                                            # 输出
